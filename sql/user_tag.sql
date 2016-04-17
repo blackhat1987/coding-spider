aa1b027ace6e59f2cf8d98b90dadd6247a466e2b
LincolnZhou <875199116@qq.com>
2016-04-17 12:11:23 +0800

    初始化代码

--- sql/user_tag.sql
+++
@@ -1,9 +0,0 @@
--- ----------------------------
--- Table structure for user_tag
--- ----------------------------
-DROP TABLE IF EXISTS `user_tag`;
-CREATE TABLE `user_tag` (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Id号',
-  `name` varchar(50) NOT NULL COMMENT '标签名',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=50 DEFAULT CHARSET=utf8;
